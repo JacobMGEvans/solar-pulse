68f992136b032d05bfa9a5aa62a5fffeffcc8410
Jacob MG Evans <jacobmgevans@gmail.com>
2024-08-02 16:52:41 -0700

    Add styling

---
+++ src/components/ag-grid.tsx
@@ -0,0 +1,178 @@
+'use client';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import {
+  type ColDef,
+  type GetRowIdFunc,
+  type GetRowIdParams,
+  type ValueFormatterFunc,
+  type ValueGetterParams,
+} from '@ag-grid-community/core';
+import { ModuleRegistry } from '@ag-grid-community/core';
+
+import '@ag-grid-community/styles/ag-grid.css';
+import '@ag-grid-community/styles/ag-theme-quartz.css';
+import { AdvancedFilterModule } from '@ag-grid-enterprise/advanced-filter';
+import { GridChartsModule } from '@ag-grid-enterprise/charts-enterprise';
+import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
+import { ExcelExportModule } from '@ag-grid-enterprise/excel-export';
+import { FiltersToolPanelModule } from '@ag-grid-enterprise/filter-tool-panel';
+import { MenuModule } from '@ag-grid-enterprise/menu';
+import { RangeSelectionModule } from '@ag-grid-enterprise/range-selection';
+import { RichSelectModule } from '@ag-grid-enterprise/rich-select';
+import { RowGroupingModule } from '@ag-grid-enterprise/row-grouping';
+import { SetFilterModule } from '@ag-grid-enterprise/set-filter';
+import { SparklinesModule } from '@ag-grid-enterprise/sparklines';
+import { StatusBarModule } from '@ag-grid-enterprise/status-bar';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+
+import { getData } from './fake-data';
+import { AgGridReact } from 'ag-grid-react';
+
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  AdvancedFilterModule,
+  ColumnsToolPanelModule,
+  ExcelExportModule,
+  FiltersToolPanelModule,
+  GridChartsModule,
+  MenuModule,
+  RangeSelectionModule,
+  RowGroupingModule,
+  SetFilterModule,
+  RichSelectModule,
+  StatusBarModule,
+  SparklinesModule,
+]);
+
+const numberFormatter: ValueFormatterFunc = ({ value }) => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'decimal',
+    maximumFractionDigits: 2,
+  });
+  return value === null ? '' : formatter.format(value);
+};
+
+export default function FinanceExample({
+  gridTheme = 'ag-theme-quartz',
+  isDarkMode = false,
+}) {
+  const [rowData, setRowData] = useState(getData());
+  const gridRef = useRef<AgGridReact>(null);
+
+  const colDefs = useMemo<ColDef[]>(
+    () => [
+      {
+        field: 'ticker',
+        cellRenderer: 'agGroupCellRenderer',
+        minWidth: 380,
+      },
+      {
+        field: 'instrument',
+        cellDataType: 'text',
+        type: 'rightAligned',
+        maxWidth: 180,
+      },
+      {
+        headerName: 'P&L',
+        cellDataType: 'number',
+        type: 'rightAligned',
+        cellRenderer: 'agAnimateShowChangeCellRenderer',
+        valueGetter: ({ data }: ValueGetterParams) =>
+          data && data.quantity * (data.price / data.purchasePrice),
+        valueFormatter: numberFormatter,
+        aggFunc: 'sum',
+      },
+      {
+        headerName: 'Total Value',
+        type: 'rightAligned',
+        cellDataType: 'number',
+        valueGetter: ({ data }: ValueGetterParams) =>
+          data && data.quantity * data.price,
+        cellRenderer: 'agAnimateShowChangeCellRenderer',
+        valueFormatter: numberFormatter,
+        aggFunc: 'sum',
+      },
+      {
+        field: 'quantity',
+        cellDataType: 'number',
+        type: 'rightAligned',
+        valueFormatter: numberFormatter,
+        maxWidth: 150,
+      },
+      {
+        headerName: 'Price',
+        field: 'purchasePrice',
+        cellDataType: 'number',
+        type: 'rightAligned',
+        valueFormatter: numberFormatter,
+        maxWidth: 150,
+      },
+      {
+        field: 'purchaseDate',
+        cellDataType: 'dateString',
+        type: 'rightAligned',
+        hide: true,
+      },
+      {
+        headerName: 'Last 24hrs',
+        field: 'last24',
+        cellRenderer: 'agSparklineCellRenderer',
+        cellRendererParams: {
+          sparklineOptions: {
+            line: {
+              strokeWidth: 2,
+            },
+          },
+        },
+      },
+    ],
+    []
+  );
+
+  const defaultColDef: ColDef = useMemo(
+    () => ({
+      flex: 1,
+      filter: true,
+      enableRowGroup: true,
+      enableValue: true,
+    }),
+    []
+  );
+
+  const getRowId = useCallback<GetRowIdFunc>(
+    ({ data: { ticker } }: GetRowIdParams) => ticker,
+    []
+  );
+
+  const statusBar = useMemo(
+    () => ({
+      statusPanels: [
+        { statusPanel: 'agTotalAndFilteredRowCountComponent' },
+        { statusPanel: 'agTotalRowCountComponent' },
+        { statusPanel: 'agFilteredRowCountComponent' },
+        { statusPanel: 'agSelectedRowCountComponent' },
+        { statusPanel: 'agAggregationComponent' },
+      ],
+    }),
+    []
+  );
+
+  return (
+    <>
+      <AgGridReact
+        ref={gridRef}
+        getRowId={(row) => getRowId(row as any)}
+        rowData={rowData}
+        columnDefs={colDefs as any}
+        defaultColDef={defaultColDef as any}
+        enableRangeSelection
+        enableCharts
+        rowSelection={'multiple'}
+        rowGroupPanelShow={'always'}
+        suppressAggFuncInHeader
+        groupDefaultExpanded={-1}
+        statusBar={statusBar}
+      />
+    </>
+  );
+}
